8676ad22119df52455ad4588b4a52c441d2c4598
Lucas M. T. <90627091+lmtlucas@users.noreply.github.com>
2026-06-09 20:44:59 -0300

    Add files via upload

---
+++ atividade_inserir_conteudo/js.js
@@ -0,0 +1,20 @@
+let usuarios = ["Lucas", "ana", "maria"];
+usuarios.pop(); // remove ultimo elemento
+usuarios.shift(); // remove o primeiro elemento
+usuarios.push("Eduardo"); // insere elemnto no final da lista
+usuarios.unshift("pedro"); //insere elemento no inicio
+
+let lista = document.getElementById("lista"); //cria variavel armazenando o elemneto id html que será alterado/manipulado
+
+lista.innerHTML = `` //limpando a lista
+
+usuarios.forEach(function (nome) { // percorre elementos da lista retornando cada elemento(nome)
+    //lista.innerHTML += `<li>${nome} </li>`;// insere html a cada iteração , usando o incremento += para adicionar cada elemento
+    //modo mais seguro:
+    let itemLista = document.createElement("li"); //criação de um novo elemento
+        itemLista.textContent = nome; // inserção da variavel referente aos elementos da lista
+        lista.appendChild(itemLista); // inserção do html criado na posição do id lista
+
+    
+});
+
