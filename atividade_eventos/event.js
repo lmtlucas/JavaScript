774a3d0703f63bbdd4222a27fd5711d60517f605
Lucas M. T. <90627091+lmtlucas@users.noreply.github.com>
2026-06-11 21:29:09 -0300

    Add files via upload

---
+++ atividade_eventos/event.js
@@ -0,0 +1,48 @@
+const botao = document.getElementById("btn");
+const quadro = document.getElementById("quadro");
+
+function reagirAoClique(){
+    quadro.style.backgroundColor = "red";
+}
+
+botao.addEventListener("click", reagirAoClique);
+
+
+// movimentando o quadrado
+let x = 0
+let y = 0
+document.addEventListener("keydown", function(e){
+    console.log(e.key);
+switch (e.key) {
+    case "ArrowRight":
+        x += 30
+         quadro.style.left = x + "px";
+        break;
+    case "ArrowDown":
+        y += 30;
+        quadro.style.top = y + "px";
+        break;
+    case "ArrowUp":
+        y -= 30;
+        quadro.style.top = y + "px";
+        break;
+    case "ArrowLeft":
+        x -= 30;
+        quadro.style.left = x + "px";
+        break;
+
+    default:
+        break;
+}
+});
+
+
+//
+const botaoDark = document.getElementById("btn-dark");
+const body =document.querySelector("body");
+
+function mudarThema(){
+    body.classList.toggle("dark-theme");
+}
+
+botaoDark.addEventListener("click", mudarThema);
